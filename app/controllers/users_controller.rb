cb5b0b0d7ca084e390d9ad186d7075bf2269052c
Anthony <tien.dau@dounets.com>
2018-11-21 18:05:33 +0700

    refactor app with text shout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < Clearance::UsersController
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts.map do |shout|
+      ShoutPresenter.new shout
+    end
+  end
+  
   private
 
   def user_params
