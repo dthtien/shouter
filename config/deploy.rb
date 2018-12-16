007b4a82480dacdeba9888879b8fac13d4d265f2
Anthony <tien.dau@dounets.com>
2018-12-16 08:12:38 +0700

    added nginx config

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,17 +40,25 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secureaftet
-after 'deploy', 'deploy:cleanup'
 after 'deploy', 'puma:config'
+after 'puma:config', 'nginx:restart' 
 
 namespace :puma do
   desc 'Config puma'
   task :config do
     on roles :web do
-      with current_path do
-        execute 'mv config/puma_example.rb config/puma.rb'
-        execute 'pumactl -P /home/deploy/shouter/shared/pids/puma.pid restart'
+      execute "mv #{current_path}/config/puma_example.rb #{current_path}/config/puma.rb"
+      within current_path do
+        execute :bundle, :exec, 'pumactl -P /home/deploy/shouter/shared/pids/puma.pid restart'
       end
+    end
+  end
+end
+
+namespace :nginx do
+  taks :restart do
+    on roles :web do
+      execute 'sudo service nginx restart'
     end
   end
 end
