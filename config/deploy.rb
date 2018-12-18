9847c35d75c44d3eb132205e87c748d131cc4700
Anthony <tien.dau@dounets.com>
2018-12-18 15:52:22 +0700

    chang nginx task

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secureaftet
 after 'deploy', 'puma:config'
-# after 'deploy', 'nginx:restart'
+after 'deploy', 'nginx:restart'
 after 'deploy', 'sidekiq:restart'
 
 namespace :puma do
@@ -60,7 +60,7 @@ namespace :nginx do
   task :restart do
     on roles :all do
       as :root do
-        execute 'service nginx restart'
+        execute "#{sudo} service nginx restart"
       end
     end
   end
